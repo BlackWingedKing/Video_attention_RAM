233d0e537d2380398438de35632c6260917c7fc1
BlackWingedKing <ritesh.goru@gmail.com>
2019-12-29 16:41:42 +0530

    c3d about to do

--- c3d_model.py
+++ c3d_model.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from mypath import Path
 
 class C3D(nn.Module):
     """
@@ -10,15 +9,15 @@ class C3D(nn.Module):
     def __init__(self, num_classes, pretrained=False):
         super(C3D, self).__init__()
 
-        self.conv1 = nn.Conv3d(3, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv1 = nn.Conv3d(1, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
 
         self.conv2 = nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        self.pool2 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(1, 2, 2))
 
         self.conv3a = nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.conv3b = nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1))
-        self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(1, 2, 2))
 
         self.conv4a = nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.conv4b = nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1))
@@ -44,27 +43,52 @@ class C3D(nn.Module):
     def forward(self, x):
 
         x = self.relu(self.conv1(x))
+        print(x.shape)
         x = self.pool1(x)
+        print(x.shape)
 
         x = self.relu(self.conv2(x))
+        print(x.shape)
+
         x = self.pool2(x)
+        print(x.shape)
 
         x = self.relu(self.conv3a(x))
+        print(x.shape)
+
         x = self.relu(self.conv3b(x))
+        print(x.shape)
+
         x = self.pool3(x)
+        print(x.shape)
 
         x = self.relu(self.conv4a(x))
+        print(x.shape)
+
         x = self.relu(self.conv4b(x))
+        print(x.shape)
+
         x = self.pool4(x)
+        print(x.shape)
 
         x = self.relu(self.conv5a(x))
+        print(x.shape)
+
         x = self.relu(self.conv5b(x))
+        print(x.shape)
+
         x = self.pool5(x)
+        print(x.shape)
 
         x = x.view(-1, 8192)
         x = self.relu(self.fc6(x))
+        print(x.shape)
+
         x = self.dropout(x)
+        print(x.shape)
+
         x = self.relu(self.fc7(x))
+        print(x.shape)
         x = self.dropout(x)
 
         logits = self.fc8(x)
@@ -146,8 +170,8 @@ def get_10x_lr_params(model):
                 yield k
 
 if __name__ == "__main__":
-    inputs = torch.rand(1, 3, 16, 112, 112)
-    net = C3D(num_classes=101, pretrained=True)
+    inputs = torch.rand(1, 1, 6, 112, 112)
+    net = C3D(num_classes=101, pretrained=False )
 
     outputs = net.forward(inputs)
     print(outputs.size())
---
+++ copy_folder_name.py
@@ -0,0 +1,27 @@
+import os 
+# for (root,dirs,files) in os.walk('../'): 
+#     print (root )
+    # print (dirs )
+    # print (files )
+    # print ('--------------------------------')
+
+# def walklevel(some_dir="../data/UCF-101", level=1):
+#     print("num_sep")
+#     some_dir = some_dir.rstrip(os.path.sep)
+#     assert os.path.isdir(some_dir)
+#     num_sep = some_dir.count(os.path.sep)
+#     for root, dirs, files in os.walk(some_dir):
+#         yield root, dirs, files
+#         num_sep_this = root.count(os.path.sep)
+#         if num_sep + level <= num_sep_this:
+#             del dirs[:]
+
+# print("b")
+# a =walklevel(some_dir="../data/UCF-101", level=1)
+
+a =os.listdir("../data/UCF-101/UCF-101")
+for z in a:
+	print(z)
+	os.mkdir("../data/UCF-101/out/{}".format(str(z)))
+	# print(os.cwd())
+
